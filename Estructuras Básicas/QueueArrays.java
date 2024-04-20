55e69eea57cab6b3e4d45cf9c5530e16a29c40d9
Gabriel <gabriel.gonzalezl@outlook.com>
2024-04-19 23:43:18 -0400

    new file:   "Estructuras B\303\241sicas/LinkedList.java" modified:   "Estructuras B\303\241sicas/QueueArrays.java" modified:   "Estructuras B\303\241sicas/StacksLnkdList.java"

--- Estructuras Básicas/QueueArrays.java
+++ Estructuras Básicas/QueueArrays.java
@@ -5,32 +5,32 @@ public class QueueArrays<T> {
     private ArrayList<T> queue;
     
     public QueueArrays() {
-        queue = new ArrayList<>();
+        queue = new ArrayList<>();   // Se crea un ArrayList vacío
     }
     
-    public void enqueue(T element) {
+    public void enqueue(T element) { // Se añade un elemento al final de la cola
         queue.add(element);
     }
     
-    public T dequeue() {
+    public T dequeue() {             // Se elimina el primer elemento de la cola
         if (isEmpty()) {
             throw new IllegalStateException("La cola está vacía!");
         }
         return queue.remove(0);
     }
     
-    public T peek() {
+    public T peek() {               // Se obtiene el primer elemento de la cola
         if (isEmpty()) {
             throw new IllegalStateException("La cola está vacía!");
         }
         return queue.get(0);
     }
     
-    public boolean isEmpty() {
+    public boolean isEmpty() {      // Se verifica si la cola está vacía
         return queue.isEmpty();
     }
     
-    public int size() {
+    public int size() {             // Se obtiene el tamaño de la cola
         return queue.size();
     }
 }
