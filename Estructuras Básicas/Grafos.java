96396cd2c3162b841707da7e9307acf395bcaaa9
Gabriel <125526388+NuggetNuclear@users.noreply.github.com>
2024-07-08 22:11:13 -0400

    Update Grafos.java

--- Estructuras Básicas/Grafos.java
+++ Estructuras Básicas/Grafos.java
@@ -2,19 +2,33 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Grafos {
+
+    // Clase de una arista
+    
     public record Arista(String destino, int costo) {
     }
 
+    // HashMap con la lista de adyacencia (Almacena la información del nodo y una lista de conexiones con otros nodos (aristas)).
+    
     private HashMap<String, ArrayList<Arista>> listaAdyacencia;
+
+    // Constructor
 
     public Grafos(String destino, int costo)  {
         listaAdyacencia = new HashMap<>();
     }
 
+    // Método que crea una arista (con su respectivo origen, destino y costo).
+
     public void agregarConexion(String origen, String destino, int costo) {
+
+        // ComputeIfAbsent: Si ya existe conexion, no crea nuevamente, si no, la crea).
+        
         listaAdyacencia.computeIfAbsent(origen, k -> new ArrayList<>()).add(new Arista(destino, costo));
         listaAdyacencia.computeIfAbsent(destino, k -> new ArrayList<>()).add(new Arista(origen, costo));
     }
+
+    // Getter de todas las arista de un nodo.
 
     public ArrayList<Arista> obtenerConexiones(String vertice) {
         return listaAdyacencia.getOrDefault(vertice, new ArrayList<>());
@@ -23,6 +37,8 @@ public class Grafos {
     public ArrayList<String> obtenerVertices() {
         return new ArrayList<>(listaAdyacencia.keySet());
     }
+
+    // Imprimir la información del grafo.
 
     public void imprimirGrafo() {
         System.out.println("Grafo:");
